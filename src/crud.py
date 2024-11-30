d03e530df873480b7ed9013fe9d05cf3e2664347
LamVanDi <lamvandi11@gmail.com>
2024-11-30 14:50:51 +0700

    Update crud.py

--- src/crud.py
+++ src/crud.py
@@ -1,6 +1,5 @@
 import pandas as pd
-import tkinter as tk
-from tkinter import ttk, messagebox, simpledialog
+from tkinter import messagebox, simpledialog
 
 from utils import save
 from utils import load
